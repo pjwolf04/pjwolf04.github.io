253b0ecdecc5d23e4e56d5eae0f015e8602aeb49
pjwolf04 <parkerstory26@gmail.com>
2025-10-08 01:21:44 -0500

    Add files via upload

---
+++ facts.js
@@ -0,0 +1,8 @@
+let name = "Parker Story";
+let favColor ="Red";
+let favHobby = "Playing Videogames";
+let favFood = "Enchaladias";
+console.log (name);
+console.log(favColor)
+console.log (favHobby);
+console.log(favFood);
